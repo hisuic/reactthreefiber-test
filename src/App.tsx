65cab72a72d3666014786af18e9e0fdb7c2ea093
hisui <henryharlay@gmail.com>
2025-08-16 04:10:06 +0900

    remove unneeded imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
